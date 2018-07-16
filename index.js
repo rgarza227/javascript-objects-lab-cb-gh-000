9c344f65e2bf9240ffe834958370624fe288fa04
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 13:38:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var tempObject = new Object();
-  tempObject = object
+  var tempObject = object
   tempObject[key] = value
   return tempObject
 }
